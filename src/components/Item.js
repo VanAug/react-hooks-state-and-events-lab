464064380dc026557261ff0c4ef936a27a2d97fd
Brian <okellobrian18@gmail.com>
2025-04-11 12:57:12 +0300

    states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState(false)
+
+  const handleClick = () => {
+    setAddToCart(prevState => !prevState)
+  }
+
+  const cart = addToCart ? "in-cart" : ""
+
+  const cartColor = addToCart ? "remove" : "add"
+  
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartColor} onClick={handleClick} >{addToCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("Filter by category")
+
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const filterdItems = items.filter(item => (
+    selectedCategory === "Filter by category" || item.category === selectedCategory
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select 
+          name="filter"
+          value={selectedCategory}
+          onChange={handleChange}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterdItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
